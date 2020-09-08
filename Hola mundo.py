179a0353d9ff0edb5f4f7d8ca6b77a00e1939a9d
Héctor Martínez <hectormmgdth@hotmail.com>
2020-09-07 20:03:42 -0500

    Initial commit

--- Hola mundo.py
+++ Hola mundo.py
@@ -1,3 +1,11 @@
-print("Hola mundo!")
+# This is a sample Python script.
 
-print("Adios mundo")
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('Hector')
+    print("Hola mundo!")
+    print("Adios mundo")
