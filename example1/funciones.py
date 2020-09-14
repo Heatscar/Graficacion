aaaee6dbbb14fa1a90a3770706e59387da92f649
Héctor Martínez <hectormmgdth@hotmail.com>
2020-09-14 18:27:06 -0500

    Actualizacion para agregar productos

--- example1/funciones.py
+++ example1/funciones.py
@@ -17,8 +17,18 @@ def cerrar():
 
 def imprimir():
     for fila in lector:
+        #if fila[0].startswith("nombre"):
+            #continue
         print("Nombre:", fila[0])
         print("Precio:", fila[1])
         print("Cantidad", fila[2])
     productos.seek(0)
     next(lector)
+
+def agregar(nombre, precio, cantidad):
+    cerrar()
+    #"a" es para agregar al archivo csv
+    productos = open("datos.csv", "a")
+    productos.write(nombre + "," + str(precio) + "," + str(cantidad))
+    productos.close()
+    abrir()
--- example1/main.py
+++ example1/main.py
@@ -1,9 +1,21 @@
 import funciones as f
 
-def principal():
+def agregar():
+    f.abrir()
+    nombre = input("Escribe el nombre: ")
+    precio = float(input("Escribe el precio: "))
+    cantidad = int(input("Escribe la cantidad: "))
+
+    f.agregar(nombre,precio,cantidad)
+    f.imprimir()
+    f.cerrar()
+
+def abrir_cerrar():
     f.abrir()
     f.imprimir()
     f.cerrar()
 
+
 if __name__ == '__main__':
-    principal()
+    #abrir_cerrar()
+    agregar()
