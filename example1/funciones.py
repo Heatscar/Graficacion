43914976fcafd1df745a125592ca9b32cb762263
Héctor Martínez <hectormmgdth@hotmail.com>
2020-09-14 18:50:34 -0500

    Se agrego metodo de actualizar

--- example1/funciones.py
+++ example1/funciones.py
@@ -1,4 +1,5 @@
-import csv 
+import csv
+import os
 
 productos = ""
 lector = ""
@@ -29,6 +30,22 @@ def agregar(nombre, precio, cantidad):
     cerrar()
     #"a" es para agregar al archivo csv
     productos = open("datos.csv", "a")
-    productos.write(nombre + "," + str(precio) + "," + str(cantidad))
+    productos.write(nombre + "," + str(precio) + "," + str(cantidad)+"\n")
     productos.close()
+    abrir()
+
+def actualizar(nombre, cantidad):
+    temporal = open("tmp.csv", "w")
+    productos.seek(0)
+    for fila in lector:
+        if fila[0] == nombre:
+            cantidad_final = int(fila[2]) + cantidad
+            temporal.write(fila[0] + "," + fila[1] + "," + str(cantidad_final)+"\n")
+        else:
+            temporal.write(fila[0] + "," + fila[1] + "," + str(fila[2])+"\n")
+        
+    temporal.close()
+    cerrar()
+    os.rename("tmp.csv", "datos.csv")
+    
     abrir()
