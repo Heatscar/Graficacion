1c01603f5828407cb718c397621a626beaaa6b6e
Héctor Martínez <hectormmgdth@hotmail.com>
2020-10-20 19:13:42 -0500

    Se creo el programa para detección de rostro

--- Ejercicio2/main.py
+++ Ejercicio2/main.py
@@ -1,5 +1,13 @@
 def print_hi(name):
     print(f'Hola, {name}')
 
+def suma():
+    num1 = int(input("Numero 1: "))
+    num2 = int(input("Numero 2: "))
+    print("La suma es:", num1 + num2)
+
+
+
 if __name__ == '__main__':
-    print_hi('Heatscar_')
+    # print_hi('Heatscar_')
+    suma()
